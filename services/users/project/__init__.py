24bde5821c39e2323cc5a6444dbd33f5441c0058
Matt Park <quantumdataninja@gmail.com>
2019-08-09 05:40:59 -0400

    sample data

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -5,6 +5,45 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 
+new_simple_user_dict = {'email': 'user@test.org'}
+new_simple_user_dict2 = {'email': 'user2@test.org'}
+new_user_dict = {
+    'email': 'user@test.org',
+    'subscribed': True,
+    'terms_and_conditions': True,
+    'firstname': 'first',
+    'middlename': 'middle',
+    'lastname': 'last',
+    'address1': '1523 John St',
+    'address2': None,
+    'city': 'Fort Lee',
+    'state': 'NJ',
+    'zipcode': '07024',
+    'country': 'United States',
+    'phone': None,
+    'birthmonth': 'January',
+    'birthday': '01',
+    'birthyear': '1990'
+}
+new_user_dict2 = {
+    'email': 'user2@test.org',
+    'subscribed': True,
+    'terms_and_conditions': True,
+    'firstname': 'first',
+    'middlename': 'middle',
+    'lastname': 'last',
+    'address1': '1523 John St',
+    'address2': None,
+    'city': 'Fort Lee',
+    'state': 'NJ',
+    'zipcode': '07024',
+    'country': 'United States',
+    'phone': None,
+    'birthmonth': 'January',
+    'birthday': '01',
+    'birthyear': '1990'
+}
+
 db = SQLAlchemy()
 
 
