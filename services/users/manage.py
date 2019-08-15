8cd3ff68324185846ba58ce0ebb9218674e2cf1a
Matt Park <quantumdataninja@gmail.com>
2019-08-14 23:04:39 -0400

    coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import coverage
 from flask.cli import FlaskGroup
 from project import (
     create_app, db,
@@ -9,8 +10,35 @@ from project import (
 from project.api.models import SimpleUser, User
 
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
+
+
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
 
 
 @cli.command('recreate_db')
