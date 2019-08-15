d4f5739ef868381868284f7aae12f540228d454a
Matt Park <quantumdataninja@gmail.com>
2019-08-15 00:11:30 -0400

    asdf

--- services/users/set_environment.bat
+++ services/users/set_environment.bat
@@ -1,5 +1,5 @@
 set FLASK_APP=project/__init__.py
 set FLASK_ENV=development
 set APP_SETTINGS=project.config.DevelopmentConfig
-set DATABASE_URL=postgres://postgres:postgres@usersb:5432/users_dev  # new
-set DATABASE_TEST_URL=postgres://postgres:postgres@users-db:5432/users_test  # new
+set DATABASE_URL=postgres://postgres:postgres@usersb:5432/users_dev
+set DATABASE_TEST_URL=postgres://postgres:postgres@users-db:5432/users_test
