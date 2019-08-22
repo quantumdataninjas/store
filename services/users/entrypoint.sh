00c1aa604f3b21a4240fa44291f0628535ecf3ad
Matt Park <quantumdataninja@gmail.com>
2019-08-22 03:11:49 -0400

    back to flask cli?

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -8,6 +8,12 @@ done
 
 echo "PostgreSQL started"
 
-python manage.py recreate_db
-python manage.py seed_db
-python manage.py run -h 0.0.0.0
+# python manage.py recreate_db
+# python manage.py seed_db
+# python manage.py run -h 0.0.0.0
+
+flask recreate_db
+flask seed_db
+flask run -h 0.0.0.0
+
+# gunicorn -b 0.0.0.0:5000 manage:app
