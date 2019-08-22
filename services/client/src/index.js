902329573fbd66088c2eb98f88766fe1181702e6
Matt Park <quantumdataninja@gmail.com>
2019-08-22 11:49:47 -0400

    setting up redux

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const middleware = [ thunk ];
+if(process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
