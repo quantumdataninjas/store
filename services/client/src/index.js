ea5eded040ddfa9b3c9801a00e9a869caf915b5b
Matt Park <quantumdataninja@gmail.com>
2019-08-25 10:24:15 -0400

    ;

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,45 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import { createBrowserHistory } from 'history'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from 'reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { createBrowserHistory } from 'history';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import rootReducer from 'reducers';
 import {
   getSimpleUserList,
   getAllProducts
-} from 'actions'
-import 'assets/scss/index.scss'
-import App from 'containers/App'
-import * as serviceWorker from 'serviceWorker'
+} from 'actions';
+import 'assets/scss/index.scss';
+import App from 'containers/App';
+import * as serviceWorker from 'serviceWorker';
 
 /**
 https://github.com/creativetimofficial/react-redux-tutorial/blob/master/Part%204/paper-dashboard-react/src/index.js
 
 use with react router (check above link)
-const hist = createBrowserHistory()
+const hist = createBrowserHistory();
 **/
 
-const middleware = [ thunk ]
+const middleware = [ thunk, promise ];
 if(process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
-}
+  middleware.push(createLogger());
+};
 const store = createStore(
-  reducer,
+  rootReducer,
   applyMiddleware(...middleware)
-)
-store.dispatch(getSimpleUserList())
-store.dispatch(getAllProducts())
+);
+// store.dispatch(getSimpleUserList());
+store.dispatch(getAllProducts());
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
--- services/client/src/reducers/index.js
+++ services/client/src/reducers/index.js
@@ -1,27 +1,30 @@
-import { combineReducers } from 'redux'
-import simpleUserList from './simpleUserList'
-import userList from './userList'
-import cart, * as fromCart from './cart'
-import products, * as fromProducts from './products'
+import { combineReducers } from 'redux';
+import simpleUsersReducer from './simpleUsers';
+import usersReducer from './users';
+import cartReducer, * as fromCart from './cart';
+import productsReducer, * as fromProducts from './products';
+
 
 export default combineReducers({
-  simpleUserList,
-  userList,
-  cart,
-  products
-})
+  simple_users: simpleUsersReducer,
+  users: usersReducer,
+  cart: cartReducer,
+  products: productsReducer
+});
 
-const getAddedIds = state => fromCart.getAddedIds(state.cart)
-const getQuantity = (state, id) => fromCart.getQuantity(state.cart, id)
-const getProduct = (state, id) => fromProducts.getProduct(state.products, id)
+// const getSimpleUserList = state =>
+
+const getAddedIds = state => fromCart.getAddedIds(state.cart);
+const getQuantity = (state, id) => fromCart.getQuantity(state.cart, id);
+const getProduct = (state, id) => fromProducts.getProduct(state.products, id);
 
 export const getTotal = state =>
   getAddedIds(state).reduce((total, id) =>
     total + getProduct(state, id).price * getQuantity(state, id), 0
-  ).toFixed(2)
+  ).toFixed(2);
 
 export const getCartProducts = state =>
   getAddedIds(state).map(id => ({
     ...getProduct(state, id),
     quantity: getQuantity(state, id)
-  }))
+  }));
