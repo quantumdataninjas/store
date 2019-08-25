f006b7f4e03f7f03861b8088291200144c10e8c8
Matt Park <quantumdataninja@gmail.com>
2019-08-25 10:27:34 -0400

    ; and SimpleUserListContainer -> SimpleUsersContainer Same for UserListContainer

--- services/client/src/containers/App.js
+++ services/client/src/containers/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import logo from 'assets/img/logo.svg'
-import 'assets/scss/App.scss'
-import SimpleUserListContainer from './Users/SimpleUserListContainer'
-import UserListContainer from './Users/UserListContainer'
-import SubscribeForm from './Auth/SubscribeForm'
-import ProductsContainer from './ProductsContainer'
-import CartContainer from './CartContainer'
-
+import React, { Component } from 'react';
+import logo from 'assets/img/logo.svg';
+import 'assets/scss/App.scss';
+import SimpleUsersContainer from './Users/SimpleUsersContainer';
+import UsersContainer from './Users/UsersContainer';
+import SubscribeForm from './Auth/SubscribeForm';
+import ProductsContainer from './ProductsContainer';
+import CartContainer from './CartContainer';
 
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -18,15 +18,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Store Name</h1>
         </header>
-        <SimpleUserListContainer />
-        <UserListContainer />
+        <SimpleUsersContainer />
+        <UsersContainer />
         <hr />
         <ProductsContainer />
         <hr />
         <CartContainer />
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 export default App;
