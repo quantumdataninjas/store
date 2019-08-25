b8391d507f3b9281cc00c9d1fe392b19ce95c12e
Matt Park <quantumdataninja@gmail.com>
2019-08-25 10:22:21 -0400

    simpleUserList -> simpleUsers

---
+++ services/client/src/reducers/simpleUsers.js
@@ -0,0 +1,16 @@
+import { GET_SIMPLE_USERS, SIMPLE_USER_CREATED } from 'constants/ActionTypes';
+
+
+const simpleUsersReducer = (state=[], action) => {
+  console.log("action: " + JSON.stringify(action));
+  switch(action.type) {
+    case GET_SIMPLE_USERS:
+      return [...state, ...action.simple_users];
+    case SIMPLE_USER_CREATED:
+      return [...state, ...action.simple_users];
+    default:
+      return state;
+  };
+};
+
+export default simpleUsersReducer;
