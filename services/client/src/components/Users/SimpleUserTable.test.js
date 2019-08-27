f48a6ca8ceae2e9a401c1410ad9a278d3e61f636
Matt Park <quantumdataninja@gmail.com>
2019-08-27 09:57:37 -0400

    simple user and user tests

---
+++ services/client/src/components/Users/SimpleUserTable.test.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import SimpleUserTable from './SimpleUserTable'
+
+
+const simple_users = [
+    {
+        "id": "1",
+        "email": "user@test.org",
+        "subscribed": "true",
+        "signed_up": "false",
+        "online": "true",
+        "last_signin": "8/26/2019",
+        "last_signout": "8/26/2019",
+        "created_at": "8/26/2019"
+    },
+    {
+        "id": "2",
+        "email": "user2@test.org",
+        "subscribed": "true",
+        "signed_up": "false",
+        "online": "true",
+        "last_signin": "8/26/2019",
+        "last_signout": "8/26/2019",
+        "created_at": "8/26/2019"
+    },
+    {
+        "id": "3",
+        "email": "test@test.org",
+        "subscribed": "true",
+        "signed_up": "false",
+        "online": "true",
+        "last_signin": "8/26/2019",
+        "last_signout": "8/26/2019",
+        "created_at": "8/26/2019"
+    },
+]
+
+const cols = [
+    { header: "ID", name: "id" },
+    { header: "Email", name: "email" },
+    { header: "Subscribed", name: "subscribed" },
+    { header: "Signed Up", name: "signed_up" },
+    { header: "Online", name: "online" },
+    { header: "Last Signin", name: "last_signin" },
+    { header: "Last Signout", name: "last_signout" },
+    { header: "Created At", name: "created_at" },
+]
+
+test('SimpleUserTable renders properly', () => {
+//   const wrapper = shallow(<SimpleUserTable simple_users={simple_users}/>)
+  const wrapper = mount(<SimpleUserTable simple_users={simple_users}/>)
+  const table = wrapper.find('table')
+  expect(table).toHaveLength(1)
+  const thead = table.find('thead')
+  expect(thead).toHaveLength(1)
+  const headers = thead.find('th')
+  expect(headers).toHaveLength(cols.length)
+  headers.forEach((th, i) => {
+      expect(th.text()).toEqual(cols[i].header)
+  })
+  const tbody = table.find('tbody')
+  expect(tbody).toHaveLength(1)
+  const rows = tbody.find('tr')
+  expect(rows).toHaveLength(simple_users.length)
+  rows.forEach((tr, i) => {
+      const cells = tr.find('td')
+      expect(cells).toHaveLength(cols.length)
+      cells.forEach((cell, j) => {
+          expect(cell.text()).toEqual(simple_users[i][cols[j].name])
+      })
+  })
+})
---
+++ services/client/src/components/Users/UserTable.test.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import UserTable from './UserTable'
+
+
+const users = [
+    {
+        "id": "1",
+        "email": "user@test.org",
+        "subscribed": "true",
+        "terms_and_conditions": "true",
+        "verified": "false",
+        "firstname": "first",
+        "middlename": "",
+        "lastname": "last",
+        "address1": "1523 John St.",
+        "address2": "",
+        "city": "Fort Lee",
+        "state": "NJ",
+        "zipcode": "07024",
+        "country": "United States",
+        "phone": "",
+        "birthday": "1/1/1990",
+        "online": "true",
+        "last_signin": "8/26/2019",
+        "last_signout": "8/26/2019",
+        "created_at": "8/26/2019"
+    },
+    {
+        "id": "2",
+        "email": "user2@test.org",
+        "subscribed": "true",
+        "terms_and_conditions": "true",
+        "verified": "false",
+        "firstname": "first",
+        "middlename": "",
+        "lastname": "last",
+        "address1": "1523 John St.",
+        "address2": "",
+        "city": "Fort Lee",
+        "state": "NJ",
+        "zipcode": "07024",
+        "country": "United States",
+        "phone": "",
+        "birthday": "1/1/1990",
+        "online": "true",
+        "last_signin": "8/26/2019",
+        "last_signout": "8/26/2019",
+        "created_at": "8/26/2019"
+    },
+]
+
+const cols = [
+    { header: "ID", name: "id" },
+    { header: "Email", name: "email" },
+    { header: "Subscribed", name: "subscribed" },
+    { header: "Terms And Conditions", name: "terms_and_conditions" },
+    { header: "Verified", name: "verified" },
+    { header: "First Name", name: "firstname" },
+    { header: "Middle Name", name: "middlename" },
+    { header: "Last Name", name: "lastname" },
+    { header: "Address1", name: "address1" },
+    { header: "Address2", name: "address2" },
+    { header: "City", name: "city" },
+    { header: "State", name: "state" },
+    { header: "Zipcode", name: "zipcode" },
+    { header: "Country", name: "country" },
+    { header: "Phone", name: "phone" },
+    { header: "Birthday", name: "birthday" },
+    { header: "Online", name: "online" },
+    { header: "Last Signin", name: "last_signin" },
+    { header: "Last Signout", name: "last_signout" },
+    { header: "Created At", name: "created_at" },
+]
+
+test('UserTable renders properly', () => {
+  const wrapper = mount(<UserTable users={users}/>)
+  const table = wrapper.find('table')
+  expect(table).toHaveLength(1)
+  const thead = table.find('thead')
+  expect(thead).toHaveLength(1)
+  const headers = thead.find('th')
+  expect(headers).toHaveLength(cols.length)
+  headers.forEach((th, i) => {
+      expect(th.text()).toEqual(cols[i].header)
+  })
+  const tbody = table.find('tbody')
+  expect(tbody).toHaveLength(1)
+  const rows = tbody.find('tr')
+  expect(rows).toHaveLength(users.length)
+  rows.forEach((tr, i) => {
+      const cells = tr.find('td')
+      expect(cells).toHaveLength(cols.length)
+      cells.forEach((cell, j) => {
+          expect(cell.text()).toEqual(users[i][cols[j].name])
+      })
+  })
+})
