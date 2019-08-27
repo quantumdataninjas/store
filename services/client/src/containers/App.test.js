581793f5fa47625279dedd27416725fadfea0777
Matt Park <quantumdataninja@gmail.com>
2019-08-27 11:25:39 -0400

    asdf

--- services/client/src/containers/App.test.js
+++ services/client/src/containers/App.test.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk'
 import promise from 'redux-promise'
 import { createLogger } from 'redux-logger'
 import { App } from './App'
-// import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
 import configureStore from 'redux-mock-store'
 
@@ -14,7 +14,7 @@ const logger = createLogger()
 const middlewares = [thunk, promise, logger]
 const mockStore = configureStore(middlewares)
 
-describe('App Component tests', () => {
+describe('App Component', () => {
   const initialState = {
     simple_users: [],
     users: [],
@@ -23,7 +23,7 @@ describe('App Component tests', () => {
   }
   const store = mockStore(initialState)
 
-  it('should shallow render App component', () => {
+  it('shallow renders', () => {
     const wrapper = shallow(<App store={store} />)
     const app = wrapper.find('.App')
     expect(app.length).toBe(1)
