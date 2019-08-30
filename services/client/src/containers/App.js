f8a40b9166c54a544c47aae876c70d238dfbadc7
Matt Park <quantumdataninja@gmail.com>
2019-08-30 10:42:01 -0400

    components -> containers

---
+++ services/client/src/containers/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import logo from 'assets/img/logo.svg'
+import 'assets/scss/App.scss'
+import SimpleUsersContainer from './Users/SimpleUsersContainer'
+import UsersContainer from './Users/UsersContainer'
+import SubscribeForm from 'components/Auth/SubscribeForm'
+import ProductsContainer from './ProductsContainer'
+import CartContainer from './CartContainer'
+import { Paper, Typography } from '@material-ui/core'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      palette: {
+        type: "dark"
+      }
+    }
+  }
+
+  toggleDarkTheme() {
+    let newPaletteType = this.state.palette.type === "light" ? "dark" : "light"
+    this.setState({
+      palette: newPaletteType
+    })
+  }
+
+  render() {
+    const muiTheme = createMuiTheme(this.state)
+    return (
+      <MuiThemeProvider theme={muiTheme}>
+        <div className="App">
+          <SubscribeForm />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            {/* <h1>Store Name</h1> */}
+            <Typography component="h1">
+              Store Name
+            </Typography>
+          </header>
+          <SimpleUsersContainer />
+          <UsersContainer />
+          <ProductsContainer />
+          <CartContainer />
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default App
+
+// const mapStateToProps = (state) => ({
+// })
+
+// export default connect(
+//   mapStateToProps
+// )(App)
