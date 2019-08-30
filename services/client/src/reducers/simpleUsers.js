8d67febf87589eb34e8a68e7292213e785273836
Matt Park <quantumdataninja@gmail.com>
2019-08-30 10:43:27 -0400

    console.log

--- services/client/src/reducers/simpleUsers.js
+++ services/client/src/reducers/simpleUsers.js
@@ -1,11 +1,14 @@
 import { GET_SIMPLE_USERS, SIMPLE_USER_CREATED } from 'constants/ActionTypes'
 
 
-const simple_users = (state=[], action) => {
+const initialState = []
+
+const simple_users = (state=initialState, action) => {
   switch(action.type) {
     case GET_SIMPLE_USERS:
       return [...state, ...action.simple_users]
     case SIMPLE_USER_CREATED:
+      console.log('SIMPLE_USER_CREATED')
       return [...state, ...action.simple_users]
     default:
       return state
