58e242938ab7d888a0b252366394e1ee0847cb0d
Matt Park <quantumdataninja@gmail.com>
2019-09-06 18:55:04 -0400

    removed comments

--- services/users/project/tests/test_simple_user_model.py
+++ services/users/project/tests/test_simple_user_model.py
@@ -1,5 +1,4 @@
 import unittest
-
 from project import db
 from project.api.models import SimpleUser
 from project.tests.base import BaseTestCase
@@ -8,34 +7,24 @@ from project.utils import (
     simple_user_dict,
     simple_user_dict2
 )
-
 from sqlalchemy.exc import IntegrityError
 
 
 class TestSimpleUserModel(BaseTestCase):
 
     def test_add_simple_user(self):
-        # user = SimpleUser(**simple_user_dict)
-        # db.session.add(user)
-        # db.session.commit()
         user = add_simple_user(simple_user_dict)
         self.assertTrue(user.id)
         self.assertEqual(user.email, 'user@test.org')
         self.assertTrue(user.online)
 
     def test_add_user_duplicate_email(self):
-        # user = SimpleUser(**simple_user_dict)
-        # db.session.add(user)
-        # db.session.commit()
         add_simple_user(simple_user_dict)
         duplicate_user = SimpleUser(**simple_user_dict)
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_to_json(self):
-    #   user = SimpleUser(**simple_user_dict)
-    #   db.session.add(user)
-    #   db.session.commit()
       user = add_simple_user(simple_user_dict)
       self.assertTrue(isinstance(user.to_dict(), dict))
 
