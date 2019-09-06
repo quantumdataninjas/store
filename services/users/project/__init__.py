5da408b49d26e70e027b471c92a028f37116f5d8
Matt Park <quantumdataninja@gmail.com>
2019-09-06 19:22:45 -0400

    bcrypt

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 # from flask_mail import Mail
 
 
@@ -13,6 +14,7 @@ db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
 cors = CORS()
 migrate = Migrate()
+bcrypt = Bcrypt()
 # mail = Mail()
 
 
@@ -24,7 +26,8 @@ def create_app(script_info=None):
     db.init_app(app)
     toolbar.init_app(app)
     cors.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
     # mail.init_app(app)
 
     from project.api.users import users_blueprint
