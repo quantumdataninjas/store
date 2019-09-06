7b9a236273ba3e1a0666e601980dc6de7887160d
Matt Park <quantumdataninja@gmail.com>
2019-09-06 18:44:24 -0400

    using util functions

--- services/users/project/tests/test_simple_user_model.py
+++ services/users/project/tests/test_simple_user_model.py
@@ -4,6 +4,7 @@ from project import db
 from project.api.models import SimpleUser
 from project.tests.base import BaseTestCase
 from project.utils import (
+    add_simple_user,
     simple_user_dict,
     simple_user_dict2
 )
@@ -14,25 +15,28 @@ from sqlalchemy.exc import IntegrityError
 class TestSimpleUserModel(BaseTestCase):
 
     def test_add_simple_user(self):
-        user = SimpleUser(**simple_user_dict)
-        db.session.add(user)
-        db.session.commit()
+        # user = SimpleUser(**simple_user_dict)
+        # db.session.add(user)
+        # db.session.commit()
+        user = add_simple_user(simple_user_dict)
         self.assertTrue(user.id)
         self.assertEqual(user.email, 'user@test.org')
         self.assertTrue(user.online)
 
     def test_add_user_duplicate_email(self):
-        user = SimpleUser(**simple_user_dict)
-        db.session.add(user)
-        db.session.commit()
+        # user = SimpleUser(**simple_user_dict)
+        # db.session.add(user)
+        # db.session.commit()
+        add_simple_user(simple_user_dict)
         duplicate_user = SimpleUser(**simple_user_dict)
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_to_json(self):
-      user = SimpleUser(**simple_user_dict)
-      db.session.add(user)
-      db.session.commit()
+    #   user = SimpleUser(**simple_user_dict)
+    #   db.session.add(user)
+    #   db.session.commit()
+      user = add_simple_user(simple_user_dict)
       self.assertTrue(isinstance(user.to_dict(), dict))
 
 
