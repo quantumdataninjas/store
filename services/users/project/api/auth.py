677ce6891d32975cfd09212b433f1631f8e2a8c9
Matt Park <quantumdataninja@gmail.com>
2019-09-09 16:43:27 -0400

    setting up auth

---
+++ services/users/project/api/auth.py
@@ -0,0 +1,124 @@
+from flask import Blueprint, jsonify, request
+from flask_restful import Resource, Api
+from sqlalchemy import exc, or_
+from project.api.models import (
+    SimpleUser,
+    User,
+    Address
+)
+from project import db, argon2
+from project.utils import validate_email
+
+
+auth_blueprint = Blueprint('auth', __name__)
+api = Api(auth_blueprint)
+
+
+class Signup(Resource):
+    """
+    Signup Api
+    """
+
+    def post(self):
+        post_data = request.get_json()
+        if not post_data:
+            return {
+                "message": "Invalid payload."
+            }, 400
+        try:
+            username = post_data["username"]
+            email = post_data["email"]
+            if not validate_email(email):
+                return {
+                    "message": f"{email} is not a valid email."
+                }
+            if User.query.filter_by(email=email).first():
+                return {
+                    "message": f"User {email} already exists."
+                }, 400
+            elif User.query.filter_by(username=username).first():
+                return {
+                    "message": f"User {username} already exists."
+                }, 400
+            simple_user = SimpleUser.query.filter_by(email=email).first()
+            if simple_user:
+                simple_user.signed_up = True
+            else:
+                simple_user = SimpleUser(email=email, signed_up=True)
+                db.session.add(simple_user)
+                db.session.commit()
+            post_data["simple_user_id"] = simple_user.id
+
+            new_address = Address(**post_data["address"])
+            db.session.add(new_address)
+            db.session.commit()
+            post_data["main_address_id"] = new_address.id
+            del post_data["address"]
+
+            post_data["password_hash"] = post_data["password"]
+            del post_data["password"]
+            user = User(**post_data)
+            user.set_password_hash(post_data["password_hash"])
+            db.session.add(user)
+
+            simple_user.user = user
+            user.addresses = [new_address]
+            user.address_history = [new_address.id]
+
+            db.session.commit()
+
+            auth_token = user.encode_auth_token(user.id)
+            return {
+                "message": f"{username} has signed up!",
+                "auth_token": auth_token.decode()
+            }, 201
+        except TypeError as te:
+            return {
+                "message": f"Type Error: {te}"
+            }, 400
+        except KeyError as ke:
+            return {
+                "message": f"Key Error: {ke}"
+            }, 400
+        except exc.IntegrityError as ie:
+            db.session.rollback()
+            return {
+                "message": f"Integrity Error: {ie}"
+            }, 400
+
+
+@auth_blueprint.route('/auth/login', methods=['POST'])
+class Signin(Resource):
+    """
+    Signin Api
+    """
+
+    def post(self):
+        post_data = request.get_json()
+        if not post_data:
+            return {
+                "message": "Invalid payload."
+            }
+        email = post_data["email"]
+        password = post_data["password"]
+        try:
+            user = User.query.filter_by(email=email).first()
+            # if user and bcrypt.check_password_hash(user.password_hash, password)
+            if user and argon2.check_password_hash(user.password_hash, password):
+                auth_token = user.encode_auth_token(user.id)
+                if auth_token:
+                    return {
+                        "message": "Successfully signed in.",
+                        "auth_token": auth_token.decode()
+                    }, 200
+            return {
+                "message": f"User {email} does not exist."
+            }, 404
+        except Exception as e:
+            return {
+                "message": f"Exception: {e}"
+            }, 500
+
+
+api.add_resource(Signup, "/auth/signup")
+api.add_resource(Signin, "/auth/signin")
---
+++ services/users/project/tests/test_auth.py
@@ -0,0 +1,176 @@
+import json
+import unittest
+from copy import deepcopy
+
+from project import db
+from project.api.models import (
+    SimpleUser,
+    User
+)
+from project.tests.base import BaseTestCase
+from project.utils import (
+    add_user,
+    user_dict,
+    simple_user_dict
+)
+
+
+class TestAuthBlueprint(BaseTestCase):
+    def test_user_can_signup(self):
+        """
+        Ensure a new user can signup.
+        """
+        with self.client:
+            response = self.client.post(
+                "/auth/signup",
+                data=json.dumps(user_dict),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            user = User.query.filter_by(
+                email=user_dict["email"]
+            ).first()
+            simple_user = SimpleUser.query.filter_by(
+                email=user.email
+            ).first()
+            self.assertEqual(simple_user.signed_up, True)
+            self.assertTrue(data['auth_token'])
+            self.assertIn(f"{user.username} has signed up!", data["message"])
+
+    def test_subscribed_user_record_updates_after_signup(self):
+        """
+        Ensure a subscribed user is signedup after signing up.
+        """
+        with self.client:
+            self.client.post(
+                "/users/subscribe",
+                data=json.dumps(simple_user_dict),
+                content_type="application/json",
+            )
+            simple_user = SimpleUser.query.filter_by(
+                email=simple_user_dict["email"]
+            ).first()
+            self.assertEqual(simple_user.signed_up, False)
+            response = self.client.post(
+                "/auth/signup",
+                data=json.dumps(user_dict),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(simple_user.signed_up, True)
+            self.assertIn("%s has signed up!" % user_dict["username"], data["message"])
+
+    def test_signup_with_str(self):
+        """
+        Ensure error is thrown if the Post data is not a JSON object.
+        """
+        with self.client:
+            response = self.client.post(
+                "/auth/signup",
+                data=json.dumps("not json"),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                "Type Error: string indices must be integers",
+                data["message"]
+            )
+
+    def test_signup_with_empty_json(self):
+        """
+        Ensure error is thrown if the JSON object is empty.
+        """
+        with self.client:
+            response = self.client.post(
+                "/auth/signup",
+                data=json.dumps({}),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn("Invalid payload.", data["message"])
+
+    def test_signup_with_invalid_json(self):
+        """
+        Ensure error is thrown if simple_user object is posted.
+        """
+        with self.client:
+            response = self.client.post(
+                "/auth/signup",
+                data=json.dumps(simple_user_dict),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn("Key Error: 'username'", data["message"])
+
+    def test_signup_with_invalid_email(self):
+        """
+        Ensure error is thrown if the email is invalid.
+        """
+        with self.client:
+            response = self.client.post(
+                "/users/subscribe",
+                data=json.dumps({**user_dict, "email": "invalid"}),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn("invalid is not a valid email.", data["message"])
+
+    def test_signup_with_duplicate_email(self):
+        """
+        Ensure error is thrown if the email already exists.
+        """
+        with self.client:
+            self.client.post(
+                "/auth/signup",
+                data=json.dumps(user_dict),
+                content_type="application/json",
+            )
+            response = self.client.post(
+                "/auth/signup",
+                data=json.dumps(user_dict),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                f'User {user_dict["email"]} already exists.',
+                data["message"],
+            )
+    
+
+    def test_signed_up_user_signin(self):
+        with self.client:
+            add_user(deepcopy(user_dict))
+            response = self.client.post(
+                '/auth/signin',
+                data=json.dumps(user_dict),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['message'] == 'Successfully signed in.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 200)
+
+    def test_not_signed_up_user_signin(self):
+        with self.client:
+            response = self.client.post(
+                '/auth/signin',
+                data=json.dumps(user_dict),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            email = user_dict["email"]
+            self.assertTrue(data['message'] == f'User {email} does not exist.')
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()
